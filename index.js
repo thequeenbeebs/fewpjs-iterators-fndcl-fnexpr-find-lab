fad62bad33a34153c2146b0111b4e46e89f97e3b
Blaire Baker <bakerblaire@gmail.com>
2021-01-11 15:31:33 -0600

    done

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayOfObjects) {
+  let win = arrayOfObjects.find(e => e.result === "W")
+  if (win) {
+    return win.year
+  }
+}
